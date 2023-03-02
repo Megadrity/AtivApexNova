b46c502ddb618d385f546a6664941ed40e7b7570
Adriana Davi <adritypd@gmail.com>
2023-03-02 13:53:31 -0300

    Atualizado

---
+++ Alertas/Js/Js/app.js
@@ -0,0 +1,9 @@
+const titulo1=document.querySelector("#titulo");
+console.log(titulo.getAttribute('teste'));
+
+// if(titulo.hasAttribute titulo.setAttribute('teste','agora tá come;cando a piorar')
+
+const botao =document.querySelector("#click");
+botao.onclick = function(){
+    
+}
